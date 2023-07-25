41b240962163ded150889db267dc2aa102c3edee
Frost-Bite <sitefb@gmail.com>
2023-07-25 14:47:59 +0300

    Add files via upload

---
+++ bulk-gpt-translation/README.md
@@ -0,0 +1,17 @@
+# Bulk GPT translation
+This script is designed to take input from an excel file, translate the content of a specific column using OpenAI's GPT API, and write the translated content to excel file.
+
+## Installation
+To install the necessary dependencies, run the following command:
+```
+pip install -r requirements.txt
+```
+You need to replace the API key with your own https://platform.openai.com/account/api-keys
+## Usage
+To use the Metacritic Parser, execute the following Python script:
+```
+python translate.py
+```
+## Notes
+
+Any parts of the content that were skipped during translation are also recorded in a separate column.
---
+++ bulk-gpt-translation/translate.py
@@ -0,0 +1,149 @@
+import openai
+import pandas as pd
+import re
+import time
+from datetime import datetime
+
+# Define OpenAI api key
+openai.api_key = 'sk-keyKeykey'
+
+# Define pattern for tags and shortcodes
+pattern = r'\[.*?\]|{.*?}|<\/?[a-zA-Z][^>]*>'
+
+# Define patterns for replacements
+img_pattern = r'<img[^>]*>'
+heading_open_pattern = r'<h[2-5][^>]*>'
+heading_close_pattern = r'</h[2-5]>'
+
+# Get the current date and time
+now = datetime.now()
+date_for_filename = now.strftime("%Y%m%d-%H%M")
+
+# Define filename
+filename = 'translatedresults'+date_for_filename+'.xlsx'
+
+# Function to make the API request
+def translate_text(text):
+    while True:
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "You will be provided with a sentence in a foreign language, and your task is to translate it into English."
+                    },
+                    {
+                        "role": "user",
+                        "content": text
+                    }
+                ],
+                temperature=0,
+                max_tokens=1024,
+                top_p=1.0,
+                frequency_penalty=0.0,
+                presence_penalty=0.0
+            )
+            # Get the content of the response
+            response_content = response['choices'][0]['message']['content']
+
+            # Remove the initial phrase
+            response_content = response_content.replace(
+                "Sure, I can help you with that. Please provide me with the sentence in the foreign language you would like me to translate into English.",
+                ""
+            )
+            # If the translation response is an error message, extract the original word
+            error_pattern = r"I'm sorry, but \"(.*?)\" is not a sentence in a foreign language\. It appears to be a word or a title\. Can you please provide a sentence for translation\?"
+            match = re.search(error_pattern, response_content)
+            if match:
+                response_content = match.group(1)
+            # Remove the ending phrase
+            response_content = response_content.replace("Here's your translation:", "").strip()
+            return response_content
+
+        except openai.error.RateLimitError as e:
+            print(f"Rate limit reached. Error: {str(e)}")
+            print("Rate limit reached. Waiting for 20 seconds before retrying...")
+            time.sleep(20)
+
+        except openai.error.APIError as e:
+            if e.http_status == 502:
+                print("APIError 502: Bad Gateway. Waiting for 60 seconds before retrying...")
+                time.sleep(60)
+            else:
+                raise e
+
+# Read the excel file
+df = pd.read_excel("test1.xlsx", engine='openpyxl')
+
+# Create a new columns
+df['Translated_Content'] = ""
+df['Skipped'] = ""
+
+articlestotranslate = len(list(df['Content'].items()))
+
+# Process each cell in the "Content" column
+for idx, value in df['Content'].items():
+    text = str(value)  # Convert the cell content to a string
+    if text.isspace():  # Skip if the cell is empty or contains only whitespaces
+        continue
+
+    matches = re.findall(pattern, text)  # Find all matches
+    non_matches = re.split(pattern, text)  # Find all non-matching strings
+
+    blockstotranslate = len(non_matches)
+
+    translations = []
+    skipped_parts = []
+
+    # Translate all non-matching strings
+    for index, piece in enumerate(non_matches):
+        print('translating ', index+1, '/', blockstotranslate, ' in article ', idx+1, '/', articlestotranslate)
+        # Skip if the piece is empty or ...
+        tokens = len(piece.split()) # Counting tokens as words might not be the exact way tokens are counted in the OpenAI API
+        if not piece.strip() or len(piece) < 12 or tokens > 1024:
+            translations.append(piece)
+            skipped_parts.append(piece)
+            continue
+        translation = translate_text(piece)
+        translations.append(translation)
+
+        # Delay for 10 seconds
+        time.sleep(10)
+
+    # Store skipped parts in the 'Skipped' column
+    df.at[idx, 'Skipped'] = "|".join(skipped_parts)
+
+    for i in range(len(matches)):
+        # Add newline before and after each img tag
+        if re.fullmatch(img_pattern, matches[i]):
+            matches[i] = '\n\n' + matches[i] + '\n\n'
+
+        # Add newline before opening heading tags and after closing heading tags
+        elif re.fullmatch(heading_open_pattern, matches[i]):
+            matches[i] = '\n' + matches[i]
+        elif re.fullmatch(heading_close_pattern, matches[i]):
+            matches[i] = matches[i] + '\n'
+
+    # Reconstruct the text with the translated parts and the non-translated parts
+    translated_text_list = []
+    for i in range(max(len(translations), len(matches))):
+        if i < len(translations):
+            translated_text_list.append(translations[i])
+        if i < len(matches):
+            translated_text_list.append(matches[i])
+
+    translated_text = ''.join(translated_text_list)
+
+    # Remove points before the closing header tags
+    pattern_point_header = r'\.(</h[2-5]>)'
+    translated_text = re.sub(pattern_point_header, r'\1', translated_text)
+
+    translated_text = translated_text.strip()
+    translated_text = re.sub(' +', ' ', translated_text)
+
+    # Update the cell in the DataFrame
+    df.at[idx, 'Translated_Content'] = translated_text
+
+    # Save DataFrame back to Excel
+    df.to_excel(filename, engine='xlsxwriter', index=False)
